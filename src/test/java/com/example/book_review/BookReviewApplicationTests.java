7c2d6b94e4ccd392b361bb0d16421a9ca02dfe20
Truong back-end <honguyententhanhtruong@gmail.com>
2026-05-05 18:12:39 +0700

    Update

---
+++ src/test/java/com/example/book_review/BookReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book_review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
